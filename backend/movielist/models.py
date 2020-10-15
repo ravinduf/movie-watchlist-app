8d3427497674e30033e776d8b20d627e5015f746
ravinduf <ravinduf@rocketmail.com>
2020-10-15 20:09:05 +0530

    added model Movies

--- backend/movielist/models.py
+++ backend/movielist/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Movies(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    slug = models.SlugField()
+    year_released = models.CharField(max_length=4),
+    poster = models.ImageField(upload_to='posters', blank=True, null=True)
+    watched = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        slug = slugify(self.title)
+
+        self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    
