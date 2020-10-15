4fa9817063aa0622d30bdb51e126fb3be8b09f7d
ravinduf <ravinduf@rocketmail.com>
2020-10-15 21:00:13 +0530

    customized admin panel

--- backend/movielist/admin.py
+++ backend/movielist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Movies
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ['slug']
+    fields = ('name', 'year_released', 'poster', 'watched')
+    search_fields = ['name']
+
+admin.site.register(Movies, MovieAdmin)
