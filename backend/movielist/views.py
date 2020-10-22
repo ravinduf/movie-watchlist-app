1a5bc7f618bafced5f0f41f7e8ddf356b8b325f6
ravinduf <ravinduf@rocketmail.com>
2020-10-22 10:33:44 +0530

    added MovieListView

--- backend/movielist/views.py
+++ backend/movielist/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from .models import Movies
+from .serializers import MoviesSerializer
 # Create your views here.
+
+class MovieListView(ListAPIView):
+    queryset = Movies.objects.order_by('name')
+    serializer_class = MoviesSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny, )
+
+
