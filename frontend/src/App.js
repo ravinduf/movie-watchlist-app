dd7439a9b1a8ce55855d53c02dd7da8051009df6
ravinduf <ravinduf@rocketmail.com>
2020-10-23 08:54:33 +0530

    Layout.js rendered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Layout from './Layout'
 function App() {
   return (
-    <div>
+    <Layout>
       
-    </div>
+    </Layout>
   );
 }
 
--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 const Layout = (props) => {
     return (
         <div>
-
+            <Header />
             {props.children}
         </div>
     )
