c31d2c8cfad44649fec8ce4871eda29c58232b96
ravinduf <ravinduf@rocketmail.com>
2020-10-25 09:55:39 +0530

    added basic boiler code to CompletedMovies.js

--- frontend/src/pages/CompletedMovies.js
+++ frontend/src/pages/CompletedMovies.js
@@ -1,1 +1,12 @@
 //shows completed movies
+import React from 'react'
+
+const CompletedMovies = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CompletedMovies
