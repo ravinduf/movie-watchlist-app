2a4ebeebf814305829846c6fcd93b4e315ea3236
ravinduf <ravinduf@rocketmail.com>
2020-10-25 09:53:14 +0530

    created YetToWatch.js with basic boiler code

---
+++ frontend/src/pages/YetToWatch.js
@@ -0,0 +1,12 @@
+//show movies yet to watch
+import React from 'react'
+
+const YetToWatch = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default YetToWatch
