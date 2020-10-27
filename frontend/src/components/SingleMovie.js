e2737de96a9c581388ce99c9d01ed2ee36ce59ee
ravinduf <ravinduf@rocketmail.com>
2020-10-27 14:26:33 +0530

    showing info of movie

--- frontend/src/components/SingleMovie.js
+++ frontend/src/components/SingleMovie.js
@@ -5,15 +5,24 @@ import { Card } from 'react-bootstrap'
 const SingleMovie = ({movie}) => {
     console.log(movie)
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={movie.poster} />
+        <Card style={{ width: '18rem', marginTop: '20px' }}>
+            <Card.Img variant="top" src={movie.poster} style={imgStyle}/>
             <Card.Body>
                 <Card.Title>{movie.name}</Card.Title>
-                
+                <Card.Text>
+                Year released: {movie.year_released}<br/>
+                Status: &nbsp;{movie.status ? 'Watched': 'Not yet watched' }
+                </Card.Text>
                 {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
         </Card>
     )
 }
+
+const imgStyle = {
+    width: '100%',
+    height: '60vh'
+}
+
 
 export default SingleMovie
