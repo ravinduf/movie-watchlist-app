35fb07975b4b6d4c0b5cd3e2772bb249a92b087e
ravinduf <ravinduf@rocketmail.com>
2020-10-27 11:20:55 +0530

    fixed active state changing functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,27 @@
 //Simple navbar 
-import React from 'react'
+import React, { useState } from 'react'
 import { Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     
+    const [ activeState, setActiveState ] = useState({id : 1})
+
+    const handleNavState = (id) => {
+        setActiveState({id: id})
+    }
 
     return (
             <>
                 <Nav  className="nav d-flex justify-content-around bg-dark mt-5 p-3">
                     <Nav.Item>
-                        <Link className="link active" to="/" onClick={handleActive()} >All</Link>
+                        <Link className={activeState.id === 1 ? 'link active': 'link'} to="/"  onClick={ () => {handleNavState(1)} }>All</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link className="link" to="/completed-movie-list">Completed</Link>
+                        <Link className={activeState.id === 2 ? 'link active': 'link'} to="/completed-movie-list" onClick={ () => {handleNavState(2)} }>Completed</Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Link className="link" to="/yet-to-watch-list">Not completed</Link>
+                        <Link className={activeState.id === 3 ? 'link active': 'link'} to="/yet-to-watch-list" onClick={ () => {handleNavState(3)} }>Not completed</Link>
                     </Nav.Item>
                 
                 </Nav>
