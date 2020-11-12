9cd1662099dab526b0ef9201ff448770c9948398
ravinduf <ravinduf@rocketmail.com>
2020-11-12 19:48:27 +0530

    created SearchForm.js and states

---
+++ frontend/src/components/SearchForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const SearchForm = () => {
+
+    const [params, setParams] = useState({
+        title: '',
+        year: ''
+    });
+
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchForm
