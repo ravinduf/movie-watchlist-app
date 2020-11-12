2ae6523e374e23f62e9f0453c3ce60449a67e7d3
ravinduf <ravinduf@rocketmail.com>
2020-11-12 20:38:38 +0530

    created handleChange func

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -9,20 +9,28 @@ const SearchForm = () => {
         year: ''
     });
     
+    const handleChange = (e) => {
+        setParams(params => (
+            {...params, [e.target.name]: e.target.value}
+        ))
+        console.log(params)
+    }
 
     return (
         <div>
-            <Form className="mt-4 d-flex flex-row justify-content-around" style={{width: "70%"}} onSubmit={handleSubmit}>
+            <Form className="mt-4 d-flex flex-row justify-content-around" style={{width: "70%"}} >
                 <Form.Control  
                 name="title" type="text" 
                 placeholder="Title" 
                 size="lg"
+                onChange={handleChange}
                 style={{width: '40%'}}/>
                 
                 <Form.Control  
                 name="year" type="text" 
                 placeholder="Year" 
                 size="lg"
+                onChange={handleChange}
                 style={{width: '20%'}}/>
                 
                 <Button type="submit">Submit</Button>
