e6bccb71a671467b970a6851c23282a6d17bfd57
ravinduf <ravinduf@rocketmail.com>
2020-11-13 15:44:57 +0530

    showing more info

--- frontend/src/components/SingleMovieInfo.js
+++ frontend/src/components/SingleMovieInfo.js
@@ -9,7 +9,11 @@ const SingleMovieInfo = ({movie}) => {
                 <Col>
                 <h2 className="display-4">{movie.Title}</h2>
                 <h4 className="display-5">{movie.Plot}</h4>
-        
+                <br/>
+                <h3>Ratings : </h3>
+                {movie.Ratings.map((rating,index) => (
+                    <h4 key={index} style={{paddingLeft: '40px'}}>{rating.Source}: &nbsp;{rating.Value}</h4>
+                ))}
                 </Col>
             </Row>
         </Container>
