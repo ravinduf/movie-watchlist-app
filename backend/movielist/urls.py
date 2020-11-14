5733faf0fe666e49adc8a26853367f6da3348097
ravinduf <ravinduf@rocketmail.com>
2020-11-14 10:21:14 +0530

    created url for posting data

--- backend/movielist/urls.py
+++ backend/movielist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', MovieListView.as_view())
+    path('', MovieListView.as_view()),
+    path('add-movie/', MoviePostView.as_view())
 ]
