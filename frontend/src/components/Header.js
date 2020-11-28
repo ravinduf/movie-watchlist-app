a32c60f38930cbccc2b979d27966791eeb868538
ravinduf <ravinduf@rocketmail.com>
2020-11-28 21:29:07 +0530

    icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../images/movie-reel-cinema-tool.svg'
 
 const Header = () => {
     return (
-        <div className="header bg-primary">
-            Movie WatchList Tracker
+        <div className="d-flex flex-row header bg-primary justify-content-center">
+            <img src={logo} alt="" style={{height: '50px', marginRight: '20px'}}></img>
+            <h1>Movie WatchList Tracker</h1>
         </div>
     )
 }
