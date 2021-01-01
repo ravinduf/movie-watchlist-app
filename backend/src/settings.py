db094d21f287a02c62a4c84a4da0a8ccfa5311cc
ravinduf <ravinduf@rocketmail.com>
2021-01-01 08:48:32 +0530

    commented restframework settings temporarily

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -157,14 +157,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_WHITELIST = [ 'http://localhost:3000' ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
 
 # configure Djoser
 DJOSER = {
