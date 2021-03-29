2e051929f6c518f547586e742fd5897c0f0a87ad
ravinduf <ravinduf@rocketmail.com>
2021-03-29 13:39:38 +0530

    basic layout for signup form

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const SignUpForm = () => {
-    return (
-        <div>
-            SignUpForm
-        </div>
-    )
+  return (
+    <Container className="form">
+      <h3 className="form-header">SignUp</h3>
+      <Form>
+        <Form.Group>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="email" placeholder="Enter Username" />
+        </Form.Group>
+        
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
 }
 
 export default SignUpForm
