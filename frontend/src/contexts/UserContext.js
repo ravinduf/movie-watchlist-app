c9fd2f0aa807b3fb0b26858f1cddfd1ec1d53b88
ravinduf <ravinduf@rocketmail.com>
2021-04-14 09:51:17 +0530

    completed methods for user authentification

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -6,16 +6,18 @@ export const UserContext = createContext();
 export const UserContextProvider = (props) => {
   const [user, setUser] = useState();
 
-  const userLogin = () => {
-
+  const userLogin = (username, token) => {
+    localStorage.setItem('username', username)
+    localStorage.setItem('token', token)
   }
 
   const userLogout = () => {
-
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
   }
 
   const checkUserLogin = () => {
-
+    return localStorage.getItem('token');
   }
 
   return(
