a09cdf321fca90febe1b74486d1f56d9a42884b8
ravinduf <ravinduf@rocketmail.com>
2021-04-14 09:03:05 +0530

    signin done

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,33 +1,92 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required("your name is required"),
+  email: yup
+    .string()
+    .email()
+    .required("your email is required"),
+  password: yup
+    .string()
+    .required("password is required"),
+  confirmPassword: yup
+    .string()
+    .required("confirm password is required")
+    .when("password", {
+      is: password => (password && password.length > 0 ? true : false),
+      then: yup.string().oneOf([yup.ref("password")], "Password doesn't match")
+    })
+});
+
 const SignUpForm = () => {
+
+  const history = useHistory();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = async data => {
+    // const {confirmPassword, ...userData} = data;
+    // console.log(userData);
+    const userData = { username: data.username, email: data.email, password: data.password }
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL_SIGNUP}`, userData);
+      history.push('/');
+      history.push('/user/sign-in');
+    } catch (error) {
+      
+      if(error.response.data.password){
+        alert(`${error.response.data.password}`)
+      }
+    }
+  }
   return (
-    <Container className="form">
+    <Container className="form" onSubmit={handleSubmit(onSubmit)}>
       <h3 className="form-header">SignUp</h3>
+
       <Form>
+
         <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="email"/>
+          <Form.Control {...register("username")} type="text"/>
+          <p>{errors.username?.message}</p>
         </Form.Group>
         
         <Form.Group>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email"/>
+          <Form.Control {...register("email")} type="email" />
+          <p>{errors.email?.message}</p>
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" />
+          <Form.Control {...register("password")} type="password"/>
+          <p>{errors.password?.message}</p>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control {...register("confirmPassword")} type="password"/>
+          <p>{errors.confirmPassword?.message}</p>
         </Form.Group>
         
         <Button variant="primary" type="submit">
           Submit
         </Button>
+      
       </Form>
+
     </Container>
   )
 }
