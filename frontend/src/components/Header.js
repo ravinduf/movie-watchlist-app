7311071565abe248d2a12c82f7dbfe968a14d734
ravinduf <ravinduf@rocketmail.com>
2021-04-29 21:28:26 +0530

    updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,30 +7,30 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
 
-    return (
-        <div className="d-flex flex-row header bg-primary">
-            <div className="m-auto">
-                <div className="d-flex flex-row">
-                    <img src={logo} alt="" style={{height: '50px', marginRight: '20px'}}></img>
-                    <h1>Movie WatchList Tracker</h1>
-                </div>
+  return (
+    <div className="d-flex flex-row header bg-primary">
+      <div className="m-auto">
+          <div className="d-flex flex-row">
+              <img src={logo} alt="" style={{height: '50px', marginRight: '20px'}}></img>
+              <h1>Movie WatchList Tracker</h1>
+          </div>
 
-            </div>
-            {isLogin() ? (<Dropdown className="">
-                  <Dropdown.Toggle variant="success" id="dropdown-basic">
-                      {localStorage.getItem('username')}
-                     
-                  </Dropdown.Toggle>
+      </div>
+      
+      {isLogin() ? (<Dropdown className="">
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                {localStorage.getItem('username')}
+                
+            </Dropdown.Toggle>
 
-                  <Dropdown.Menu>
-                      <Dropdown.Item href="/user/sign-in">logout</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>) : 
-                ("")}
-            
+            <Dropdown.Menu>
+                <Dropdown.Item href="/user/sign-in">logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>) : 
+          ("")}
 
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Header
