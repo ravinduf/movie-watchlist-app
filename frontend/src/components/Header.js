b065267b4ff58aebd6ff53ad737c75b3b22fec2f
ravinduf <ravinduf@rocketmail.com>
2021-04-29 20:52:36 +0530

    added a simple dropdown with username and dropdown item logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react'
+
+import isLogin from '../utils/isLogin'
 import logo from '../images/movie-reel-cinema-tool.svg'
+
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Header = () => {
     return (
-        <div className="d-flex flex-row header bg-primary justify-content-center">
-            <img src={logo} alt="" style={{height: '50px', marginRight: '20px'}}></img>
-            <h1>Movie WatchList Tracker</h1>
+        <div className="d-flex flex-row header bg-primary">
+            <div className="m-auto">
+                <div className="d-flex flex-row">
+                    <img src={logo} alt="" style={{height: '50px', marginRight: '20px'}}></img>
+                    <h1>Movie WatchList Tracker</h1>
+                </div>
+
+            </div>
+            {!isLogin ? (null) : 
+                (<Dropdown className="">
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                      {localStorage.getItem('username')}
+                     
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                      <Dropdown.Item href="/user/sign-in">logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>)}
+            
+
         </div>
     )
 }
