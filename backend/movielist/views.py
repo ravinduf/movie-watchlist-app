683cf3d605ea23e5a8b21eb9b2d545c0c0ad3e44
ravinduf <ravinduf@rocketmail.com>
2021-05-07 20:16:13 +0530

    created MovieUpdateView

--- backend/movielist/views.py
+++ backend/movielist/views.py
@@ -2,7 +2,7 @@ from django.db.models.query import QuerySet
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, UpdateAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 from django.shortcuts import get_object_or_404
@@ -11,7 +11,7 @@ import os
 from pathlib import Path
 import urllib
 from .models import Movies
-from .serializers import MoviesSerializer
+from .serializers import MoviesSerializer, MovieUpdateDeleteSerializer
 from rest_framework import viewsets
 from tempfile import NamedTemporaryFile
 
@@ -21,32 +21,39 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class MovieListView(ListAPIView):
-    serializer_class = MoviesSerializer
-    lookup_field = 'slug'
+  serializer_class = MoviesSerializer
+  lookup_field = 'slug'
 
-    def get_queryset(self):
-        user = self.request.user
-        return Movies.objects.filter(user = user)
+  def get_queryset(self):
+      user = self.request.user
+      return Movies.objects.filter(user = user)
+
+  # def perform_update(self, serializer_class):
+
     
+class MovieUpdateView(UpdateAPIView):
+  serializer_class = MovieUpdateDeleteSerializer
+  lookup_field = 'pk'
+  queryset = Movies.objects.all() 
 
 
 class MoviePostView(APIView):
-    serializer_class = MoviesSerializer
-    permissions_classes = (permissions.AllowAny, )
-    parser_classes = (MultiPartParser, FormParser)
+  serializer_class = MoviesSerializer
+  permissions_classes = (permissions.AllowAny, )
+  parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request, *args, **kwargs):
-        print(self.request.user.pk)
-        filename = request.data['name'] + '.jpg'
-        result = urllib.request.urlretrieve(request.data['poster'], filename)
-        data = {'user': self.request.user.pk ,'name': request.data['name'], 'year_released': request.data['year_released'], 'poster' : File(open(filename, 'rb'))}
-        movie_serializer = MoviesSerializer(data=data)
+  def post(self, request, *args, **kwargs):
+      print(self.request.user.pk)
+      filename = request.data['name'] + '.jpg'
+      result = urllib.request.urlretrieve(request.data['poster'], filename)
+      data = {'user': self.request.user.pk ,'name': request.data['name'], 'year_released': request.data['year_released'], 'poster' : File(open(filename, 'rb'))}
+      movie_serializer = MoviesSerializer(data=data)
 
-        if movie_serializer.is_valid():
-            movie_serializer.save()
-            os.remove(BASE_DIR / filename)
-            return Response(movie_serializer.data, status=status.HTTP_201_CREATED)
+      if movie_serializer.is_valid():
+          movie_serializer.save()
+          os.remove(BASE_DIR / filename)
+          return Response(movie_serializer.data, status=status.HTTP_201_CREATED)
 
-        else:
-            print('error', movie_serializer.errors)
-            return Response(movie_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      else:
+          print('error', movie_serializer.errors)
+          return Response(movie_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
