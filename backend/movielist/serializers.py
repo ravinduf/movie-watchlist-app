8de29c675bdcc832160682b183328aff5a4d8132
ravinduf <ravinduf@rocketmail.com>
2021-05-07 20:15:31 +0530

    created MovieUpdateDeleteSerializer

--- backend/movielist/serializers.py
+++ backend/movielist/serializers.py
@@ -6,4 +6,11 @@ class MoviesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movies
         exclude = ['slug',]
-        lookup_field = 'slug'
+        lookup_field = 'pk'
+
+class MovieUpdateDeleteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movies
+        exclude = ['slug', 'poster']
+        lookup_field = 'pk'
