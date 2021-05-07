93a70de6a0ab2688e634bf6907c1601345d6b03f
ravinduf <ravinduf@rocketmail.com>
2021-05-07 20:13:26 +0530

    added function to updateMovie

--- frontend/src/contexts/MoviesContext.js
+++ frontend/src/contexts/MoviesContext.js
@@ -5,33 +5,51 @@ import authHeader from '../utils/authheader';
 export const MoviesContext = createContext();
 
 export const MoviesContextProvider = (props) => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    const [ activeState, setActiveState ] = useState({id : 1})
+  const [ activeState, setActiveState ] = useState({id : 1})
 
-    const handleNavState = (id) => {
-        setActiveState({id: id})
-    }
+  const handleNavState = (id) => {
+      setActiveState({id: id})
+  }
 
-    const getMovies = async () => {
-      
-        try {
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/movielist`, {
-              headers: {
-                "Authorization": authHeader()
-              }
+  const getMovies = async () => {
+      try {
+          const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/movielist`, {
+            headers: {
+              "Authorization": authHeader()
             }
-            );
-            setMovies(res.data)
+          }
+          );
+          setMovies(res.data)
+      }
+      catch(err){
+          console.log(err)
+      }
+  }
+
+  const updateMovie = async ( movieToUpdate ) => {
+     
+    const tempMovies = movies.map(movie => movie.id === movieToUpdate.id ? movieToUpdate : movie)
+    setMovies(tempMovies)
+
+    try {
+      const res = await axios.put(`${process.env.REACT_APP_API_URL}/api/movielist/update-movie/${movieToUpdate.id}`, movieToUpdate, {
+        headers: {
+          "Authorization": authHeader()
         }
-        catch(err){
-            console.log(err)
-        }
+      })
+
+      console.log('res')
+    }
+    catch (err){
+      console.log(err);
     }
 
-    return(
-        <MoviesContext.Provider value={{movies, setMovies, getMovies, handleNavState, activeState}}>
-            {props.children}
-        </MoviesContext.Provider>
-    )
+  }
+  return(
+      <MoviesContext.Provider value={{movies, setMovies, getMovies, updateMovie}}>
+          {props.children}
+      </MoviesContext.Provider>
+  )
 } 
