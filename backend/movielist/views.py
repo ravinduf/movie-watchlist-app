d58d67ccfbff1123a1e9c787cfcef90ce7cca04d
ravinduf <ravinduf@rocketmail.com>
2021-05-07 22:36:09 +0530

    created MovieDeleteView

--- backend/movielist/views.py
+++ backend/movielist/views.py
@@ -2,7 +2,7 @@ from django.db.models.query import QuerySet
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 from django.shortcuts import get_object_or_404
@@ -12,13 +12,12 @@ from pathlib import Path
 import urllib
 from .models import Movies
 from .serializers import MoviesSerializer, MovieUpdateDeleteSerializer
-from rest_framework import viewsets
-from tempfile import NamedTemporaryFile
+# from rest_framework import viewsets
+# from tempfile import NamedTemporaryFile
 
 
 # Create your views here.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 class MovieListView(ListAPIView):
   serializer_class = MoviesSerializer
@@ -28,13 +27,17 @@ class MovieListView(ListAPIView):
       user = self.request.user
       return Movies.objects.filter(user = user)
 
-  # def perform_update(self, serializer_class):
 
-    
 class MovieUpdateView(UpdateAPIView):
   serializer_class = MovieUpdateDeleteSerializer
   lookup_field = 'pk'
   queryset = Movies.objects.all() 
+
+
+class MovieDeleteView(DestroyAPIView):
+  serializer_class = MovieUpdateDeleteSerializer
+  lookup_field = 'pk'
+  queryset = Movies.objects.all()
 
 
 class MoviePostView(APIView):
