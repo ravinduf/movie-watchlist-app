1e49d7fe4cd49b6e28fc6d47ae4a007d826682c6
ravinduf <ravinduf@rocketmail.com>
2021-05-07 22:36:36 +0530

    url to delete

--- backend/movielist/urls.py
+++ backend/movielist/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', MovieListView.as_view()),
     path('add-movie/', MoviePostView.as_view()),
-    path('update-movie/<int:pk>', MovieUpdateView.as_view())
+    path('update-movie/<int:pk>', MovieUpdateView.as_view()),
+    path('delete-movie/<int:pk>', MovieDeleteView.as_view())
 ]
