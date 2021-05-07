3cb7d26b534b49591ef3f3bccb3d77e8c0e164c4
ravinduf <ravinduf@rocketmail.com>
2021-05-07 20:16:26 +0530

    added url to updated

--- backend/movielist/urls.py
+++ backend/movielist/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', MovieListView.as_view()),
-    path('add-movie/', MoviePostView.as_view())
+    path('add-movie/', MoviePostView.as_view()),
+    path('update-movie/<int:pk>', MovieUpdateView.as_view())
 ]
